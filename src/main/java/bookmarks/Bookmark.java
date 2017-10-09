b2de5be75d237d433747b900312993eead5113da
Dale Culpin <dale.culpin@moneysupermarket.com>
2017-10-09 12:20:44 +0100

    initial commit

---
+++ src/main/java/bookmarks/Bookmark.java
@@ -0,0 +1,43 @@
+package bookmarks;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Bookmark {
+
+    @JsonIgnore
+    @ManyToOne
+    private Account account;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    Bookmark() { /* jpa only */ }
+
+    public Bookmark(Account account, String uri, String description) {
+        this.uri = uri;
+        this.description = description;
+        this.account = account;
+    }
+
+    public String uri;
+    public String description;
+
+    public Account getAccount() {
+        return this.account;
+    }
+
+    public String getUri() {
+        return this.uri;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+}
